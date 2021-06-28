4532483206dfaba015ef8e0c0232d08939510ec9
ryu32 <76839510+ryu32@users.noreply.github.com>
2021-06-28 16:56:20 +0700

    Update ryufun.py

--- userbot/modules/ryufun.py
+++ userbot/modules/ryufun.py
@@ -47,20 +47,8 @@ async def _(event):
     await tap[0].click(event.chat_id)
     await event.delete()
     
-    
-    @register(outgoing=True, pattern=r"^\.rgame(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    botusername = "@gamee"
-    noob = "play"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.inline_query(botusername, noob)
-    await tap[0].click(event.chat_id)
-    await event.delete()
-
 # Ported For Lord-Userbot By liualvinas/Alvin
+
 
 CMD_HELP.update({
     "ryufun": "\
