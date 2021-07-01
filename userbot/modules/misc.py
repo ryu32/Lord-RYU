2bf0785d9dc9ebf2dfd80d5b36963a1cf7b3a2f5
ryu32 <76839510+ryu32@users.noreply.github.com>
2021-07-01 20:26:48 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "❃ **Repo Userbot:** [Lord-Userbot](https://github.com/Zora24/Lord-Userbot)\n❃ **Pemilik:** [Alvin](t.me/liualvinas)"
+        "❃ **Repo Userbot:** [Lord-Userbot](https://github.com/ryu32/Lord-Ryu)\n❃ **Pemilik:** [ROIF](t.me/X_Roif)"
     )
 
 
